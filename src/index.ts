d6f221a22316efdfddf27aed905c27f6ec89f010
ALEKSEY <andrsmek@gmail.com>
2025-07-23 11:46:59 +0300

    fixed server4

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@ const bot = new TelegramBot(token, { polling: true });
 const PORT = 8000;
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
